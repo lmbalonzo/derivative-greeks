3d8ff81aa29da49e5ecd5a29d28292999e2e541f
Lea Alonzo <leamonicaalonzo@gmail.com>
2022-03-22 11:31:41 +0800

    first commit

---
+++ optiongreeks.py
@@ -0,0 +1,91 @@
+from py_vollib.black_scholes import black_scholes as bs
+from py_vollib.black_scholes.greeks.analytical import delta,gamma, vega, theta, rho
+
+import numpy as np
+from scipy.stats import norm
+
+r=0.01
+S=30
+K=40
+T=240/365
+sigma=0.30
+
+def blackScholes(r,S,K,T,sigma, type="c"):
+    "Calculate BS price of call/put"
+    d1 = (np.log(S/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        if type =="c":
+            price = S*norm.cdf(d1,0,1) - K*np.exp(-r*T)*norm.cdf(d2,0,1)
+        elif type == "p":
+            price = K*np.exp(-r*T)*norm.cdf(-d2, 0 ,1) - S*norm.cdf(-d1,0,1)
+        return price, bs(type, S, K, T, r, sigma)
+    except:
+        print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def delta_calc(r,S,K,T,sigma, type="c"):
+    "Calculate Delta of an option"
+    d1 = (np.log(S/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        if type =="c":
+            delta_calc = norm.cdf(d1,0,1)
+        elif type == "p":
+            delta_calc = norm.cdf(-d1,0,1)
+        return delta_calc, delta(type, S, K, T, r, sigma)
+    except:
+        print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def gamma_calc(r,S,K,T,sigma, type="c"):
+    "Calculate gamma of an option"
+    d1 = (np.log(S/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        gamma_calc = norm.pdf(d1,0,1)/(S*sigma*np.sqrt(T))
+        return gamma_calc, gamma(type, S, K, T, r, sigma)
+    except:
+        print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def vega_calc(r,S,K,T,sigma, type="c"):
+    "Calculate vega of an option"
+    d1 = (np.log(S/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        vega_calc = S*norm.pdf(d1,0,1)*np.sqrt(T)
+        return vega_calc*0.01, vega(type, S, K, T, r, sigma)
+    except:
+        print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def theta_calc(r,S,K,T,sigma, type="c"):
+    "Calculate theta of an option"
+    d1 = (np.log(S/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        if type =="c":
+            theta_calc = -S*norm.pdf(d1,0,1)*sigma/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*norm.cdf(d2,0,1)
+        elif type == "p":
+            theta_calc = -S*norm.pdf(d1,0,1)*sigma/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*norm.cdf(-d2,0,1)
+        return theta_calc/365, theta(type, S, K, T, r, sigma)
+    except:
+        print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+def rho_calc(r,S,K,T,sigma, type="c"):
+    "Calculate rho of an option"
+    d1 = (np.log(S/K) + (r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        if type =="c":
+            rho_calc = K*T*np.exp(-r*T)*norm.cdf(d2,0,1)
+        elif type == "p":
+            rho_calc = -K*T*np.exp(-r*T)*norm.cdf(-d2,0,1)
+        return rho_calc*0.01, rho(type, S, K, T, r, sigma)
+    except:
+        print("Please confirm option type, either 'c' for Call or 'p' for Put!")
+
+option_type="c"
+print("Option Price: ", [round(x,3) for x in blackScholes(r,S,K,T,sigma,option_type)])
+print("Option Delta: ", [round(x,3) for x in delta_calc(r,S,K,T,sigma,option_type)])
+print("Option Gamma: ", [round(x,3) for x in gamma_calc(r,S,K,T,sigma,option_type)])
+print("Option Vega: ", [round(x,3) for x in vega_calc(r,S,K,T,sigma,option_type)])
+print("Option Theta: ", [round(x,3) for x in theta_calc(r,S,K,T,sigma,option_type)])
+print("Option Rho: ", [round(x,3) for x in rho_calc(r,S,K,T,sigma,option_type)])
